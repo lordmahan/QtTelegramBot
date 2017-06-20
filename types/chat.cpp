86340a187399777f98796a97406386ce5e3f8ade
Mahan <mahan.rafiei@yahoo.com>
2017-06-21 01:31:02 +0430

    Update chat.cpp
    Added Supergroup support.

--- types/chat.cpp
+++ types/chat.cpp
@@ -4,11 +4,13 @@ using namespace Telegram;
 
 Chat::Chat(QJsonObject chat)
 {
-    id = chat.value("id").toInt();
+    id = chat.value("id").toVariant().toLongLong();
     QString chatType = chat.value("type").toString();
     if (chatType == "private") type = Private;
     else if (chatType == "group") type = Group;
     else if (chatType == "channel") type = Channel;
+    else if (chatType == "supergroup") type = Supergroup;
+    
     username = chat.value("username").toString();
     firstname = chat.value("first_name").toString();
     lastname = chat.value("last_name").toString();
